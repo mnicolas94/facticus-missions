9c9ef65b693c08bdc0466fe52ec0a8189ac3fbf4
mnicolas94 <miguelcok27@gmail.com>
2025-05-19 17:57:07 -0400

    feat: add reference to localized string variables in MissionScriptableObject's OnValidate method

--- Runtime/MissionScriptableObject.cs
+++ Runtime/MissionScriptableObject.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Localization.SmartFormat.PersistentVariables;
 
 namespace Missions
 {
     [CreateAssetMenu(fileName = "Mission", menuName = "Facticus/Missions/Mission", order = 0)]
     public class MissionScriptableObject : ScriptableObject
     {
+        private const string DescriptionSelfKey = "self";
+        
         [SerializeReference, SubclassSelector] private IMission _mission;
 
         public IMission Mission => _mission;
+
+#if UNITY_EDITOR
+        
+        private void OnValidate()
+        {
+            var description = _mission.GetDescription();
+            if (!description.ContainsKey(DescriptionSelfKey))
+            {
+                description.Add(DescriptionSelfKey, new ObjectVariable(){ Value = this});
+                EditorUtility.SetDirty(this);
+            }
+        }
+        
+#endif
     }
 }

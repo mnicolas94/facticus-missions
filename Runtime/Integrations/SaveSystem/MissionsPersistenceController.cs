5f1db3bb5c8fc0e73bbe47c5e973c2e50f0a7268
mnicolas94 <miguelcok27@gmail.com>
2025-06-17 13:08:22 -0400

    feat: add cooldown to MissionsPersistenceController.cs to avoid saving too often

--- Runtime/Integrations/SaveSystem/MissionsPersistenceController.cs
+++ Runtime/Integrations/SaveSystem/MissionsPersistenceController.cs
@@ -9,7 +9,13 @@ namespace Missions.Integrations.SaveSystem
     public class MissionsPersistenceController : MonoBehaviour
     {
         [SerializeField, AutoProperty] private MissionsManager _missionsManager;
+        
+        [Tooltip("Cooldown in seconds to avoid saving too often due to missions that progress to rapidly")]
+        [SerializeField] private float _saveCooldown = 1f;
+        
         private MissionsSerializableState _missionsList;
+        private float _lastSaveTime;
+        private bool _isOnCooldown;
         
         private void Start()
         {
@@ -32,13 +38,18 @@ namespace Missions.Integrations.SaveSystem
         public void UnregisterListeners()
         {
             _missionsList.Added -= OnMissionAdded;
-            _missionsList.Removed -= SaveList;
+            _missionsList.Removed -= OnMissionRemoved;
             _missionsList.Cleared -= SaveList;
         }
 
         private void OnMissionAdded(MissionData mission)
         {
             RegisterOnProgressChanged(mission);
+            SaveList();
+        }
+
+        private void OnMissionRemoved(MissionData mission)
+        {
             SaveList();
         }
 
@@ -50,12 +61,36 @@ namespace Missions.Integrations.SaveSystem
             }
         }
 
+        /// <summary>
+        /// Saves a mission's progress changed. If the request to save is too often, the save will be performed after
+        /// a cooldown
+        /// </summary>
         private void SaveListOnProgressChanged()
         {
-            // TODO add a cooldown to avoid to many saves for missions that progress to often
+            var elapsed = Time.time - _lastSaveTime;
+            
+            if (elapsed < _saveCooldown)  // trying to save too early
+            {
+                if (!_isOnCooldown)
+                {
+                    SaveAfterCooldown(_saveCooldown);  // put mission on cooldown
+                }
+                return;
+            }
+
             SaveList();
         }
 
+        private async void SaveAfterCooldown(float cooldown)
+        {
+            _isOnCooldown = true;
+            
+            await Awaitable.WaitForSecondsAsync(cooldown);
+            SaveList();
+            
+            _isOnCooldown = false;
+        }
+        
         private void SaveList(MissionData _)
         {
             SaveList();
@@ -63,6 +98,7 @@ namespace Missions.Integrations.SaveSystem
         
         private async void SaveList()
         {
+            _lastSaveTime = Time.time;
             await _missionsList.Save();
         }
     }

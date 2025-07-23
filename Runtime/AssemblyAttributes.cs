e4061c530f942e695cbecf5d6c2cafd1e1c706bf
mnicolas94 <miguelcok27@gmail.com>
2025-07-23 13:38:14 -0400

    fix: mark assembly with GeneratePropertyBagsForAssembly so property bags are generated for serializable classes

---
+++ Runtime/AssemblyAttributes.cs
@@ -0,0 +1,5 @@
+﻿using Unity.Properties;
+
+// tells Unity's source generator system to look through your assembly for any types marked with [GeneratePropertyBag]
+// and generate the corresponding code. See https://docs.unity3d.com/6000.0/Documentation/Manual/property-bags.html
+[assembly: GeneratePropertyBagsForAssembly]

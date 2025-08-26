edc3c77cdc440d3cacfc3b162c88a80c2cd4128e
mnicolas94 <miguelcok27@gmail.com>
2025-08-26 12:17:24 -0400

    fix: remove MissionsUI._missionsList field

--- Runtime/UI/MissionsUI.cs
+++ Runtime/UI/MissionsUI.cs
@@ -9,24 +9,20 @@ namespace Missions.UI
     public class MissionsUI : MonoBehaviour
     {
         [SerializeField] private ViewList _viewList;
-        [SerializeField] private MissionsSerializableState _missionsList;
         [SerializeReference, SubclassSelector] private IMissionListSource _missionListSource;
 
         private void Start()
         {
             UpdateMissionsList();
             _missionListSource.ListChanged += UpdateMissionsList;
-            // _missionsList.Added += AddMissionView;
-            // _missionsList.Removed += RemoveMissionView;
-            // _missionsList.Cleared += ClearMissions;
         }
 
         private void UpdateMissionsList()
         {
             _viewList.Clear();
-            foreach (var serializableMission in _missionsList.Missions)
+            foreach (var mission in _missionListSource.GetMissions())
             {
-                AddMissionView(serializableMission.Mission);
+                AddMissionView(mission);
             }
         }
 
@@ -44,16 +40,5 @@ namespace Missions.UI
         {
             _viewList.Clear();
         }
-
-#if UNITY_EDITOR
-        private void OnValidate()
-        {
-            if (_missionListSource == null)
-            {
-                _missionListSource = new SerializedMissionsSource(new() { _missionsList });
-                EditorUtility.SetDirty(this);
-            }
-        }
-#endif
     }
 }
